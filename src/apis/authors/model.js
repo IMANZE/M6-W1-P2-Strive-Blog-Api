d487299eea99102c1dadbde73cd9de348e9a67aa
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-09-01 20:25:22 +0200

    hashed password

--- src/apis/authors/model.js
+++ src/apis/authors/model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema, model } = mongoose;
 
@@ -7,8 +8,33 @@ const authorSchema = new Schema(
     firstName: { type: String },
     lastName: { type: String },
     avatar: { type: String },
+    password: { type: String },
+    email: { type: String },
   },
   { timestamps: true }
 );
+
+authorSchema.pre("save", async function (next) {
+  const author = this;
+
+  const hashPassword = author.password;
+
+  if (author.isModified("password")) {
+    const hash = await bcrypt.hash(hashPassword, 10);
+
+    author.password = hash;
+  }
+  next();
+});
+
+authorSchema.methods.toJSON = function () {
+  const authorsDetails = this;
+  const authorObject = authorsDetails.toObject();
+
+  delete authorObject.password;
+  delete authorObject.__v;
+
+  return authorObject;
+};
 
 export default model("Author", authorSchema);
